8405f25b5134b8987632220cb4b67dd2bc579807
BARTEK9046 <bartek9046b.w@gmail.com>
2023-04-27 14:16:13 +0200

    vsdg

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<RownanieKwadratowe> rownania = new ArrayList<>();
+
+        rownania.add(new RownanieKwadratowe(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble()));
+        rownania.add(new RownanieKwadratowe(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble()));
+        rownania.add(new RownanieKwadratowe(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble()));
+        rownania.add(new RownanieKwadratowe(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble()));
+
+        System.out.println(rownania.get(3).toString());
+        Double suma = rownania.get(0).getA() + rownania.get(0).getB() + rownania.get(0).getC();
+
+        Function<Double,Boolean> parzyste = (Double x) -> x%2==0;
+        System.out.println(parzyste.apply(suma));
+
+        rownania.sort(new Comparator<RownanieKwadratowe>() {
+            @Override
+            public int compare(RownanieKwadratowe o1, RownanieKwadratowe o2) {
+                return 0;
+            }
+        });
+
+
+
     }
 }
--- src/RownanieKwadratowe.java
+++ src/RownanieKwadratowe.java
@@ -1,2 +1,76 @@
-package PACKAGE_NAME;public class RownanieKwadratowe {
+/**
+ * Opis: Klasa ktora przyjmuje 3 liczby a,b,c, posiada metode oblicz(), metody get(), zmodyfikowanego ToString()
+   oraz equals i hashCode
+ * @a, @b, @c : <liczby typu double> przechowuja liczby wykorzytywane w metodach
+ * @oblicz() : <List> metoda wyliczajaca delte, sprawdza czy delta jest mniejsza od zera to zwraca pusta liste, jesli
+    delta == 0 to dodaje do listy pierwiastek i zwraca liste, w ostatnim przypadku gdy delta > 0 wylicza 2 pierwiastki
+    dodaje je do listy i zwraca liste
+ * @ToString(): <typ String> metoda zwraca rownanie
+ */
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+
+public class RownanieKwadratowe {
+    private double a,b,c;
+
+    public RownanieKwadratowe(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public static List oblicz(double a, double b, double c)
+    {
+        List list = new List();
+        double pierwiastek;
+        double delta = b*b - 4*a*c;
+        if (delta < 0) {
+            return list;
+        }
+        else if (delta == 0)
+        {
+            pierwiastek = -b/2* a;
+            list.add(String.valueOf(pierwiastek));
+        }
+        else
+        {
+            pierwiastek = -b+Math.sqrt(delta) / 2 * a;
+            double piewr2 = -b-Math.sqrt(delta) / 2 * a;
+            list.add(String.valueOf(pierwiastek));
+            list.add(String.valueOf(piewr2));
+        }
+        return list;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RownanieKwadratowe that)) return false;
+        return Double.compare(that.getA(), getA()) == 0 && Double.compare(that.getB(), getB()) == 0 && Double.compare(that.getC(), getC()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getA(), getB(), getC());
+    }
+
+    @Override
+    public String toString() {
+        return "RownanieKwadratowe{" + getA() + "x^2 +" + getB()+ "x + " + getC()+'}';
+    }
 }
